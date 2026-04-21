f48210b30c7b0daf78cc931ae4e66f49be6eeda9
张正明 <7812383+xiaoyekanren@users.noreply.github.com>
2026-04-21 10:48:36 +0800

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export DOWNLOAD_FOLDER=/data/nas
+export DOWNLOAD_FOLDER=/data
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 LOG_DIR="$ROOT_DIR/logs"
